5ad5d3a478174185bc66b8a6feffe1c6e0953d7b
Kostya Borkovets <borkoveck6@gmail.com>
2023-10-09 02:08:21 +0300

    Register models in admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import Customer, Category, SubCategory, Product, Order, OrderItem, Shipping
 
 models_list = [Order, OrderItem, Shipping]
 prepopulated_models = [Product, Category, SubCategory]
 
 
-class CategoriesAdmin(admin.ModelAdmin):
+class SlugsAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ["name"]}
 
 
@@ -14,6 +15,36 @@ for model in models_list:
     admin.site.register(model)
 
 for model in prepopulated_models:
-    admin.site.register(model, CategoriesAdmin)
+    admin.site.register(model, SlugsAdmin)
 
-admin.site.register(Customer)
+
+class CustomerAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "phone")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
+
+admin.site.register(Customer, CustomerAdmin)
--- shop/models.py
+++ shop/models.py
@@ -48,6 +48,7 @@ class Customer(AbstractUser):
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
     email = models.EmailField(_("email address"), unique=True)
+    phone = models.CharField(_("phone"), max_length=20, blank=True)
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
