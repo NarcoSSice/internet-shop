b89e289c6d01bd58812759c5b91f15bd39850b9e
Kostya Borkovets <borkoveck6@gmail.com>
2023-10-20 07:36:21 +0300

    rename register.html on login.html, changes files

--- shop/static/shop/js/registration_script.js
+++ shop/static/shop/js/registration_script.js
@@ -1,5 +1,5 @@
-$('.popup-bg').click(function (e) {
-    if (!$(e.target).closest(".popup").length) {
-      $('.popup-bg').fadeOut(600);
+$('.auth-form-bg').click(function (e) {
+    if (!$(e.target).closest(".auth-form").length) {
+      $('.auth-form-bg').fadeOut(600);
     }
   });
