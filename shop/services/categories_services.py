432f2a8cd02429effe4b8f8cc0180af71b6d9b2b
Kostya Borkovets <borkoveck6@gmail.com>
2025-02-08 08:51:18 +0200

    finall optimize queries

--- shop/services/categories_services.py
+++ shop/services/categories_services.py
@@ -5,14 +5,14 @@ from shop.models import Category, SubCategory, Product
 
 def get_subcategories(model_slug):
     category = get_object_or_404(Category, slug=model_slug)
-    categories = SubCategory.objects.filter(super_category=category.pk)
+    categories = SubCategory.objects.select_related('super_category').filter(super_category=category.pk)
 
     return categories
 
 
 def get_product_by_subcategory(subcategory_slug):
-    subcategory = SubCategory.objects.get(slug=subcategory_slug)
-    products = Product.objects.filter(subcategory=subcategory.pk)
+    subcategory = SubCategory.objects.select_related('super_category').get(slug=subcategory_slug)
+    products = Product.objects.select_related('subcategory__super_category').filter(subcategory=subcategory.pk)
 
     return products, subcategory
 
